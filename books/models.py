fc105fe8b556187f2a94bc72741f34c1b5aa5439
Julia Bengert <yubengert@gmail.com>
2025-04-09 17:05:38 +0300

    add models User, Book, Author

--- books/models.py
+++ books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    patronymic_name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000)
+
+
+class Book(models.Model):
+
+    language_choices = {
+        "eng": "Английский",
+        "ru": "Русский",
+    }
+
+    serial_number = models.CharField(unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000)
+    author = models.ManyToManyField(Author)
+    genre = models.CharField(max_length=255)
+    language = models.CharField(choices=language_choices)
+    publishing_house = models.CharField(max_length=255)
+    place_of_publication = models.CharField(max_length=50)
+    publication_year = models.PositiveIntegerField(max_length=4)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = ["username"]
+    USERNAME_FIELD = "email"
