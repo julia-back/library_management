144095aa6d1dbe09f94d6338b9ee1f875250a1a4
Julia Bengert <yubengert@gmail.com>
2025-04-24 15:34:58 +0300

    fix urls

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     path("user/new/", views.UserCreateAPIView.as_view(), name="user_create"),
     path("user_list/", views.UserListAPIView.as_view(), name="user_list"),
-    path("user/<int:pk>/", views.UserRetrieveAPIView.as_view(), name="user_retrieve"),
-    path("user/<int:pk>/", views.UserUpdateAPIView.as_view(), name="user_update"),
-    path("user/<int:pk>/", views.UserDestroyAPIView.as_view(), name="user_destroy"),
+    path("user_retrieve/<int:pk>/", views.UserRetrieveAPIView.as_view(), name="user_retrieve"),
+    path("user_update/<int:pk>/", views.UserUpdateAPIView.as_view(), name="user_update"),
+    path("user_destroy/<int:pk>/", views.UserDestroyAPIView.as_view(), name="user_destroy"),
 ]
