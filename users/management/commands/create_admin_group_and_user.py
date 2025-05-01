5ee0e6fb40906f51b1a201bcddcf7609b5bdb94e
Julia Bengert <yubengert@gmail.com>
2025-05-01 16:39:44 +0300

    fix script for create admin

--- users/management/commands/create_admin_group_and_user.py
+++ users/management/commands/create_admin_group_and_user.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import Group
 from users.models import User
 import os
 from dotenv import load_dotenv
+import logging
 
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -15,6 +17,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         admin_group, created = Group.objects.get_or_create(name="admin")
         admin_user, created = User.objects.get_or_create(email=os.getenv("ADMIN_USER_EMAIL"),
-                                                         username=os.getenv("ADMIN_USER_USERNAME"),
-                                                         password=os.getenv("ADMIN_USER_PASSWORD"))
-        admin_user.groups.add(admin_group)
+                                                         username=os.getenv("ADMIN_USER_USERNAME"))
+        admin_user.set_password(os.getenv("ADMIN_USER_PASSWORD"))
+        admin_user.save()
+        if not admin_user.groups.filter(name="admin").exists():
+            admin_user.groups.add(admin_group)
